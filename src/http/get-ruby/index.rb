50318785c65df911328eceb2ec492c55300710e2
Taylor Beseda <tbeseda@gmail.com>
2022-03-10 13:24:30 -0700

    use require_relative

--- src/http/get-ruby/index.rb
+++ src/http/get-ruby/index.rb
@@ -2,9 +2,9 @@
 require 'architect/functions'
 
 # require 'architect/shared/authenticate'
-require './vendor/shared/authenticate'
+require_relative './vendor/shared/authenticate'
 # require 'architect/views/document'
-require './vendor/views/document'
+require_relative './vendor/views/document'
 
 def handler(request, context = {})
   { body: 'shiny rock' }
